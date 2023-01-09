e4b7b092d6f5cff65f982938b7cd6af5e7d0b7b8
Adam Sutiak <adam.sutiak@gmail.com>
2023-01-09 19:44:42 +0100

    Safety

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.purpurmc.org/snapshots")
+    maven("https://repo.dmulloy2.net/repository/public/")
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     compileOnly("org.purpurmc.purpur", "purpur-api", "1.19.2-R0.1-SNAPSHOT")
-
-
+    compileOnly("com.comphenix.protocol", "ProtocolLib", "4.7.0")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/eu/ansquare/petfix/Main.java
+++ src/main/java/eu/ansquare/petfix/Main.java
@@ -1,16 +1,34 @@
 package eu.ansquare.petfix;
+import org.bukkit.Bukkit;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import me.neznamy.tab.api.TabAPI;
-import me.neznamy.tab.api.TabFeature;
-import me.neznamy.tab.api.TabPlayer;
-import me.neznamy.tab.platforms.bukkit.nms.storage.NMSStorage;
-import me.neznamy.tab.platforms.bukkit.nms.datawatcher.DataWatcher;
-import me.neznamy.tab.platforms.bukkit.nms.datawatcher.DataWatcherItem;
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable(){
+        getServer().getPluginManager().registerEvents(new ClickkinEvent(), this);
 
-import java.util.ConcurrentModificationException;
-import java.util.List;
-import java.util.Optional;
-import java.util.WeakHashMap;
-public class Main {
+        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.ENTITY_METADATA) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+//                PacketContainer packet = event.getPacket().deepClone();
+//                event.setPacket(packet);
+                System.out.println("packet");
+            }
+        });
 
+
+    }
 }
