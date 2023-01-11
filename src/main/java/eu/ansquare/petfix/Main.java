b0801e9d734de0da68e4a60a233530cf3c2c3ae6
Adam Sutiak <adam.sutiak@gmail.com>
2023-01-11 12:59:51 +0100

    Version 1.0

--- src/main/java/eu/ansquare/petfix/Main.java
+++ src/main/java/eu/ansquare/petfix/Main.java
@@ -9,6 +9,8 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.EnumWrappers;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -19,6 +21,9 @@ import java.util.List;
 
 public class Main extends JavaPlugin
 {
+    boolean burning;
+    boolean glowing;
+
     public enum EntityData {
 
         ON_FIRE((byte) 0x01),
@@ -53,6 +58,8 @@ public class Main extends JavaPlugin
     }
     @Override
     public void onEnable(){
+        burning = false;
+        glowing = false;
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
         manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.ENTITY_METADATA) {
             @Override
@@ -61,29 +68,32 @@ public class Main extends JavaPlugin
                 int entityId = event.getPacket().getIntegers().read(0); // Get the entity id.
                 Player player = getPlayer(entityId); // Will be null if the entity is not a Player
                 Player clientPlayer = event.getPlayer();
-                Object removed = null;
                 if (player == clientPlayer) return;
                 List<WrappedWatchableObject> metadata = packet.getWatchableCollectionModifier().read(0);
                 WrappedWatchableObject wrappedowner = metadata.stream().filter(obj -> obj.getIndex() == 18).findAny().orElse(null);
-//                if (bitMaskContainer == null) return;
-//                byte bitMask = (byte) bitMaskContainer.getValue();
+                WrappedWatchableObject bitMaskContainer = metadata.stream().filter(obj -> obj.getIndex() == 0).findAny().orElse(null);
 
-//                bitMask = EntityData.GLOWING.setBit(bitMask);
-//                bitMask = EntityData.SPRINTING.unsetBit(bitMask);
-//                bitMask = EntityData.ON_FIRE.setBit(bitMask);
+                if (bitMaskContainer == null) return;
+                byte bitMask = (byte) bitMaskContainer.getValue();
+                if(burning == true){
+                    bitMask = EntityData.ON_FIRE.setBit(bitMask);
 
-//                bitMaskContainer.setValue(bitMask);
-                if(wrappedowner == null) return;
-                if(wrappedowner.getValue() instanceof Optional<?> || wrappedowner.getValue() instanceof com.google.common.base.Optional<?>) {
+                }
+                if(glowing == true){
+                    bitMask = EntityData.GLOWING.setBit(bitMask);
 
-                    System.out.println("marked");
-                    wrappedowner.setValue(Optional.empty());
-                    System.out.println(wrappedowner.getValue());
-                }else{
-
-                    return;
                 }
 
+                bitMaskContainer.setValue(bitMask);
+                if(wrappedowner != null) {
+
+                    if (wrappedowner.getValue() instanceof Optional<?> || wrappedowner.getValue() instanceof com.google.common.base.Optional<?>) {
+
+                        System.out.println("marked");
+                        wrappedowner.setValue(Optional.empty());
+                        System.out.println(wrappedowner.getValue());
+                    }
+                }
 
                 event.setPacket(packet);
 
@@ -92,6 +102,41 @@ public class Main extends JavaPlugin
 
 
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        if(label.equalsIgnoreCase("ill")) {
+            if (args[0].equalsIgnoreCase("burn")) {
+                if(burning == false){
+                    burning = true;
+                    sender.sendMessage("It burns now");
+                    return true;
+                }
+                else {
+                    burning = false;
+                    sender.sendMessage("It no longer burns");
+                    return true;
+                }
+            } else if (args[0].equalsIgnoreCase("glow")) {
+                if(glowing == false){
+                    glowing = true;
+                    sender.sendMessage("It glows now");
+                    return true;
+                }
+                else {
+                    glowing = false;
+                    sender.sendMessage("It bo longer glows");
+                    return true;
+                }
+            } else {
+                sender.sendMessage("Unrecognized argument - " + args[0]);
+                return true;
+            }
+
+        }
+        return false;
+    }
+
     private Player getPlayer(final int entityID) {
         for (final Player player : Bukkit.getServer().getOnlinePlayers()) {
             if (player.getEntityId() == entityID) return player;
